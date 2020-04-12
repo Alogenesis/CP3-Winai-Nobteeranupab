a16927750a5ca37d266f04df4b9343de9046c46c
Alogenesis <63358133+Alogenesis@users.noreply.github.com>
2020-04-12 21:58:54 +0700

    Add files via upload

---
+++ Assignment/Exercise4_Winai_N.py
@@ -0,0 +1,11 @@
+F_English = 86
+G_Business = 74
+Intro_Com = 91
+Programming = 98
+
+print("--- Your Score ---")
+print("Foundation English : ", F_English)
+print("General Business : ", G_Business)
+print("Introduction to Computer Systems : ", Intro_Com)
+print("Computer Programming : ", Programming)
+
