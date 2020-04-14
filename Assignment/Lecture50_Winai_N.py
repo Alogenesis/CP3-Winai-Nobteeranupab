f7dfd401f2484dc64734aac5ba5013a5817bf19f
Alogenesis <nononosolo@hotmail.com>
2020-04-15 01:07:13 +0700

    Lecture50 Winai_N function def

---
+++ Assignment/Lecture50_Winai_N.py
@@ -0,0 +1,18 @@
+def plus(main, plusNumber):
+    print(main+plusNumber)
+
+def negative(main, negativeNumber):
+    print(main - negativeNumber)
+
+def multiply(main, multiplier):
+    print(main * multiplier)
+
+def divide(main, divider):
+    print(main / divider)
+
+print("+ - * / with 20 and 5")
+plus(20,5)
+negative(20,5)
+multiply(20,5)
+divide(20,5)
+
