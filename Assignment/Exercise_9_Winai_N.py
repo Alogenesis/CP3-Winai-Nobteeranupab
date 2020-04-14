587321a7b114413f7390efd37dd2c112ae6a785e
Alogenesis <nononosolo@hotmail.com>
2020-04-14 08:41:06 +0700

    While Loop Log in

---
+++ Assignment/Exercise_9_Winai_N.py
@@ -0,0 +1,6 @@
+userNameInput = input("Username : ")
+passwordInput = input("Password : ")
+while userNameInput != "aaaa" or passwordInput != "aaaa":
+    userNameInput = input("Username : ")
+    passwordInput = input("Password : ")
+print("Log in complete")
