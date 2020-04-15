85d94a5ee3e2555b9fb30347cc90c7170b22aad0
Alogenesis <nononosolo@hotmail.com>
2020-04-15 12:33:28 +0700

    Lecture54_Winai_N (HaveBug)
    Bug on derf menuSelect()
    when choose Menu 2 the program will repeat menu selection before go next

---
+++ Assignment/Lecture54_Winai_N.py
@@ -0,0 +1,42 @@
+def login():
+    userNameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if userNameInput == "aaaa" and passwordInput == "aaaa":
+        return True
+    else:
+        return False
+
+def showMenu():
+        print("---Log in complete---")
+        print("-----iShop Menu------")
+        print("1. Vat Calculator")
+        print("2. Total Price included Vat")
+
+def menuSelect():
+    menuselected = int(input("Menu number : "))
+    return menuselected
+def vatCalMenu():
+    totalPrice = int(input("Enter your price to calculate Vat included price .."))
+    vat = 7                                       
+    result = totalPrice + (totalPrice * vat / 100)
+    print("Vat included price is",result,"THB")
+def vatCalculate(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+def priceCalculate():
+    price1 = int(input("Price of menu 1 = ? "))
+    price2 = int(input("Price of menu 2 = ? "))
+    print("Included Vat price are ..",vatCalculate(price1+price2),"THB")
+
+#WorkFlow
+if login():
+    showMenu()
+    if int(menuSelect()) == 1:
+        vatCalMenu()
+    elif int(menuSelect()) == 2:
+        priceCalculate()
+else:
+    print("Invalid ID or Password")
+
+print("End Program")
